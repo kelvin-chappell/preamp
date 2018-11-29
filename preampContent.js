497eea1e34bb24865e09157ff06b19500f7e506d
Kelvin Chappell <kelvin.chappell@theguardian.com>
2018-11-29 14:37:49 +0000

    Hide details box initially

--- preampContent.js
+++ preampContent.js
@@ -2,6 +2,7 @@ function log(message) {
   console.log("PREAMP: " + message)
 }
 
+const zIndex = "5";
 let detailBoxes = [];
 let targetingIndex = 0;
 let adDetailsIndex = 0;
@@ -9,12 +10,21 @@ let adDetailsIndex = 0;
 const ads = document.body.getElementsByTagName("amp-ad");
 for (let i = 0; i < ads.length; i++) {
   ads[i].style.border = "5px solid red";
-  detailBoxes.push(addDetailBox(ads[i]));
+  const btn = addChildElt("button", ads[i].parentNode);
+  btn.style.width = "100px";
+  btn.style.height = "20px";
+  btn.textContent = "Show details";
+  btn.onclick = function (event) {
+    detailBoxes[i].style.display = "block"
+  };
+  detailBoxes.push(addDetailBox(ads[i], i));
 }
 
-function addDetailBox(adSlot) {
+function addDetailBox(adSlot, idx) {
   const adContainer = adSlot.parentNode;
   const newDiv = document.createElement("div");
+  newDiv.id = "adDetails" + idx;
+  newDiv.style.display = "none";
   newDiv.style.position = "absolute";
   newDiv.style.top = "auto";
   newDiv.style.left = "40px";
@@ -24,7 +34,7 @@ function addDetailBox(adSlot) {
   newDiv.style.border = "5px solid #DFDFDF";
   newDiv.style.padding = "5px 5px 5px 5px";
   newDiv.style.lineHeight = "10px";
-  newDiv.style.zIndex = "5";
+  newDiv.style.zIndex = zIndex;
   adContainer.insertBefore(newDiv, null);
   return newDiv;
 }
@@ -60,16 +70,16 @@ function addBlock(title, json, targetElt) {
   if (Object.keys(json).length === 0) {
     const p = addChildElt("p", div);
     p.style.fontSize = "14px";
-    p.textContent = "No values";
+    p.textContent = "No bids";
   }
 }
 
 function addTargeting(json, targetElt) {
-  addBlock("Targeting from bids:", json, targetElt);
+  addBlock("Bids:", json, targetElt);
 }
 
 function addAdDetails(json, targetElt) {
-  addBlock("Showing ad:", json, targetElt);
+  addBlock("Ad:", json, targetElt);
 }
 
 function showMessage(message) {
